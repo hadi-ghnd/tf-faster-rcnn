0c62bd77fcbd28e42e04a33c7d6ec8dbb8f80569
hadi-ghnd <hadi.ghahremannezhad@gmail.com>
2017-11-16 08:31:25 +0330

    Update generate_anchors.py

--- lib/layer_utils/generate_anchors.py
+++ lib/layer_utils/generate_anchors.py
@@ -38,7 +38,7 @@ import numpy as np
 #       [ -79., -167.,   96.,  184.],
 #       [-167., -343.,  184.,  360.]])
 
-def generate_anchors(base_size=16, ratios=[0.5, 1, 2],
+def generate_anchors(base_size=2, ratios=[1],
                      scales=2 ** np.arange(3, 6)):
   """
   Generate anchor (reference) windows by enumerating aspect ratios X
